b609189386e71656a8e4448b2cb7cccde8de26d2
amukoaterry <amukoaterrymitchell@gmail>
2024-10-15 17:12:44 +0300

    Made changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
-<<<<<<< Updated upstream
-import Image from "next/image";
-// import DashboardOverview from "./component/Overview"
-import PracticalsPage from "./component/Practicals"
-  // import SelectionPage from "./component/Selection";
+import Navbar from "@/app/components/Navbar";
+import Homepage from "@/app/components/Homepage";
+
 
 export default function Home() {
   return (
     <div>
       <main>
-    {/* <SelectionPage/> */}
-    {/* <DashboardOverview/> */}
-    <PracticalsPage/>
+      <Navbar/>
+      <Homepage/>
       </main>
     </div>
   );
@@ -18,17 +15,3 @@ export default function Home() {
 
 
 
-=======
-import Navbar from "@/app/components/Navbar";
-import Homepage from "@/app/components/Homepage";
-
-
-export default function Home(){
-  return(
-    <div>
-      <Navbar/>
-      <Homepage/>
-    </div>
-  );
-}
->>>>>>> Stashed changes
